d659ce72ff3e3f18e1df4f5e79b787f2b1de1f52
Benny Rahmat <akunbeben@gmail.com>
2021-12-04 07:12:52 +0700

    fix: ensure import to the right path

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useSanctum } from "react-sanctum";
 
 import Admin from "./Layouts/Admin";
-import Login from "./Pages/Auth/Login";
-import MasterBarang from "./Pages/Barang/MasterBarang";
-import NotFound from "./Pages/Errors/NotFound";
-import Dashboard from "./Pages/Home/Dashboard";
+import Login from "./Views/Auth/Login";
+import MasterBarang from "./Views/Barang/MasterBarang";
+import NotFound from "./Views/Errors/NotFound";
+import Dashboard from "./Views/Home/Dashboard";
 import { authorized, guest } from "./Util/authHelper";
 
 function App() {
