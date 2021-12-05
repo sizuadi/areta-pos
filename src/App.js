af292850344f351e3903b392fca1b386a0e9cced
Benny Rahmat <akunbeben@gmail.com>
2021-12-05 16:15:20 +0700

    refactor: change Views back to Pages

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useSanctum } from "react-sanctum";
 
 import Admin from "./Layouts/Admin";
-import Login from "./Views/Auth/Login";
-import MasterBarang from "./Views/Barang/MasterBarang";
-import NotFound from "./Views/Errors/NotFound";
-import Dashboard from "./Views/Home/Dashboard";
-import Blank from "./Views/Blank";
+import Login from "./Pages/Auth/Login";
+import MasterBarang from "./Pages/Barang/MasterBarang";
+import NotFound from "./Pages/Errors/NotFound";
+import Dashboard from "./Pages/Home/Dashboard";
+import Blank from "./Pages/Blank";
 
 import { authorized, guest } from "./Util/authHelper";
 
